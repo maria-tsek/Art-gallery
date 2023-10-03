c7185aad830fe667eae18f5857673b9e3a70661e
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-10-03 21:58:02 +0200

    just import the useImmerLocalStorageState

--- art-gallery-app/components/ArtPiecePreview/ArtPiecePreview.js
+++ art-gallery-app/components/ArtPiecePreview/ArtPiecePreview.js
@@ -47,7 +47,6 @@ export default function ArtPiecePreview({
   slug,
   isFavorite,
   onToggleFavorite,
-  imageDimensions,
 }) {
   return (
     <ArtPieceWrapper>
--- art-gallery-app/pages/_app.js
+++ art-gallery-app/pages/_app.js
@@ -34,7 +34,6 @@ export default function App({ Component, pageProps }) {
   //   });
   // }
 
-
   function handleToggleFavorite(slug) {
     mutate((pieces) => {
       const clickedPiece = pieces.find((piece) => piece.slug === slug);
@@ -49,7 +48,6 @@ export default function App({ Component, pageProps }) {
       return [...pieces, { slug, isFavorite: true }];
     }, false);
   }
-
 
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
